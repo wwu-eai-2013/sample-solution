d5429d768e63ff186b528ecfa4746260fa523a80
Jan Ernsting <jan.ernsting@gmail.com>
2013-06-28 18:39:17 +0200

    Simplify LINQ statements

--- DotNET/BusinessLayer/Logic/DrugStatisticsService.cs
+++ DotNET/BusinessLayer/Logic/DrugStatisticsService.cs
@@ -13,49 +13,26 @@ namespace Pharmacy.BusinessLayer.Logic
         {
             using (PharmacyContainer db = new PharmacyContainer())
             {
-                Dictionary<Int32, Int32> sumsFrom = GetSumOfQuantitiesPerPzn(fromDate, db);
-                Dictionary<Int32, Int32> sumsTo = GetSumOfQuantitiesPerPzn(toDate, db);
-                ICollection<Drug> allDrugs = DrugService.GetAllDrugs();
-
-                ICollection<DrugStatistic> result = new List<DrugStatistic>(allDrugs.Count);
-                foreach (Drug drug in allDrugs)
-                {
-                    result.Add(CreateDrugStatistic(drug.PZN, sumsFrom, sumsTo));
-                }
-                return result;
+                return GetAllStatistics(fromDate, toDate, db).ToList();
             }
         }
 
-        private static Dictionary<Int32, Int32> GetSumOfQuantitiesPerPzn(DateTime before, PharmacyContainer db)
+        private static IQueryable<DrugStatistic> GetAllStatistics(DateTime fromDate, DateTime toDate, PharmacyContainer db)
         {
-            var sums = from d in db.DrugSet
-                       let sumUpToBefore = d.Events.Where(e => e.DateOfAction <= before).Sum(e => e.Quantity)
-                       let sum = sumUpToBefore == null ? 0 : sumUpToBefore
-                       select new { Pzn = d.PZN, Sum = sum };
-            Dictionary<Int32, Int32> result = new Dictionary<Int32, Int32>();
-            foreach (var tuple in sums)
+            return from d in db.DrugSet
+                    let tmpSumFrom = d.Events.Where(e => e.DateOfAction <= fromDate).Sum(e => e.Quantity)
+                    let sumFrom = tmpSumFrom == null ? 0 : tmpSumFrom
+                    let tmpSumTo = d.Events.Where(e => e.DateOfAction <= toDate).Sum(e => e.Quantity)
+                    let sumTo = tmpSumTo == null ? 0 : tmpSumTo
+                    select new DrugStatistic { Pzn = d.PZN, AverageInventoryLevel = (sumFrom + sumTo) / 2.0 };
+        }
+
+        public static DrugStatistic GetStatistic(Int32 pzn, DateTime fromDate, DateTime toDate)
+        {
+            using (PharmacyContainer db = new PharmacyContainer())
             {
-                System.Diagnostics.Debug.WriteLine(String.Format("pzn: {0}, sum: {1}, date: {2}", tuple.Pzn, tuple.Sum, before));
-                result.Add(tuple.Pzn, tuple.Sum);
+                return GetAllStatistics(fromDate, toDate, db).Where(d => d.Pzn == pzn).First();
             }
-            return result;
-        }
-
-        private static DrugStatistic CreateDrugStatistic(Int32 pzn, Dictionary<Int32, Int32> sumsFrom, Dictionary<Int32, Int32> sumsTo)
-        {
-            long inventoryAtFrom = sumsFrom[pzn];
-            long inventoryAtTo = sumsTo[pzn];
-            double averageInventoryLevel = (inventoryAtFrom + inventoryAtTo) / 2.0;
-            return new DrugStatistic { Pzn = pzn, AverageInventoryLevel = averageInventoryLevel };
-        }
-
-        public static DrugStatistic GetStatistic(Int32 pzn, DateTime from, DateTime to)
-        {
-            return new DrugStatistic
-            {
-                Pzn = pzn,
-                AverageInventoryLevel = 42.5
-            };
         }
     }
 }
