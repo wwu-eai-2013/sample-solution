e9aaa5b4b3148c0451574325505a400259d829c2
Jan Ernsting <jan.ernsting@gmail.com>
2013-07-01 10:47:26 +0200

    Add retrieval of order statistics from JaVa

--- JavaEE-HO/Pharmacy-HO-EJB/ejbModule/de/java/ejb/OrderStatsServiceBean.java
+++ JavaEE-HO/Pharmacy-HO-EJB/ejbModule/de/java/ejb/OrderStatsServiceBean.java
@@ -3,20 +3,44 @@ package de.java.ejb;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
+
+import org.jboss.resteasy.client.ProxyFactory;
+import org.jboss.resteasy.plugins.providers.RegisterBuiltin;
+import org.jboss.resteasy.spi.ResteasyProviderFactory;
+
+import de.java.web.stats.order.OrderStatistic;
+import de.java.web.stats.order.OrderStatsResource;
 
 @Stateless
 public class OrderStatsServiceBean implements OrderStatsService {
 
+  static final String JAVA_SERVICE_HOST = "http://localhost:8080/Pharmacy-Web/api/";
+
+  private OrderStatsResource remote;
+
+  @PostConstruct
+  public void initialise() {
+    RegisterBuiltin.register(ResteasyProviderFactory.getInstance());
+    remote = ProxyFactory.create(OrderStatsResource.class, JAVA_SERVICE_HOST);
+  }
+
   public List<WrappedOrderStatistic> getStats() {
     ArrayList<WrappedOrderStatistic> result = new ArrayList<WrappedOrderStatistic>();
-    result.add(new WrappedOrderStatistic());
+    for (OrderStatistic stat : remote.getAllStatistics()) {
+      result.add(new WrappedOrderStatistic("JaVa", stat));
+    }
     return result;
   }
 
   @Override
   public double calculateAverageDeviation(List<WrappedOrderStatistic> stats) {
-    return 42.5;
+    long sum = 0;
+    for (WrappedOrderStatistic stat : stats) {
+      sum += stat.getDeviation();
+    }
+    return sum / stats.size();
   }
 
 }
--- JavaEE-HO/Pharmacy-HO-EJB/ejbModule/de/java/ejb/WrappedOrderStatistic.java
+++ JavaEE-HO/Pharmacy-HO-EJB/ejbModule/de/java/ejb/WrappedOrderStatistic.java
@@ -2,7 +2,29 @@ package de.java.ejb;
 
 import java.io.Serializable;
 
+import de.java.web.stats.order.OrderStatistic;
+
 public class WrappedOrderStatistic implements Serializable {
+
+  private String subsidiary;
+  private OrderStatistic statistic;
+
+  public WrappedOrderStatistic(String subsidiary, OrderStatistic statistic) {
+    this.subsidiary = subsidiary;
+    this.statistic = statistic;
+  }
+
+  public String getSubsidiary() {
+    return subsidiary;
+  }
+
+  public long getOrderId() {
+    return statistic.getOrderId();
+  }
+
+  public long getDeviation() {
+    return statistic.getDeviation();
+  }
 
   private static final long serialVersionUID = 3496548403729297148L;
 
