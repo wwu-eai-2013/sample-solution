f14b26d8d0146ca4a57305b1ca0043373d5f9986
Jan Ernsting <jan.ernsting@gmail.com>
2013-07-01 10:11:14 +0200

    Extract interface for drug statistics

--- JavaEE/Pharmacy-Web/src/de/java/web/api/DrugStatsResource.java
+++ JavaEE/Pharmacy-Web/src/de/java/web/api/DrugStatsResource.java
@@ -1,57 +1,23 @@
 package de.java.web.api;
 
-import java.util.Calendar;
 import java.util.Collection;
-import java.util.Date;
 
-import javax.ejb.EJB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.jboss.resteasy.spi.NotFoundException;
-
-import de.java.ejb.DrugService;
-import de.java.ejb.stats.AverageInventoryStatisticsService;
-
 @Path("drugstats")
-public class DrugStatsResource {
-
-  @EJB
-  private AverageInventoryStatisticsService service;
-
-  @EJB
-  private DrugService drugService;
+public interface DrugStatsResource {
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
-  public Collection<DrugStatistic> getAllStatistics() {
-    Calendar cal = Calendar.getInstance();
-    Date toDate = cal.getTime();
-    cal.add(Calendar.DAY_OF_YEAR, -30);
-    Date fromDate = cal.getTime();
-    return service.getAllStats(fromDate, toDate);
-  }
+  Collection<DrugStatistic> getAllStatistics();
 
   @GET
   @Path("{pzn}")
   @Produces(MediaType.APPLICATION_JSON)
-  public DrugStatistic getStatistic(@PathParam("pzn") int pzn) {
-
-    validateDrugExists(pzn);
-
-    Calendar cal = Calendar.getInstance();
-    Date toDate = cal.getTime();
-    cal.add(Calendar.DAY_OF_YEAR, -30);
-    Date fromDate = cal.getTime();
-    return service.getStatistic(pzn, fromDate, toDate);
-  }
-
-  private void validateDrugExists(int pzn) {
-    if (drugService.getDrug(pzn) == null)
-      throw new NotFoundException(String.format("Drug with pzn %s does not exist here", pzn));
-  }
+  DrugStatistic getStatistic(@PathParam("pzn") int pzn);
 
 }
---
+++ JavaEE/Pharmacy-Web/src/de/java/web/api/DrugStatsResourceImpl.java
@@ -0,0 +1,48 @@
+package de.java.web.api;
+
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+
+import javax.ejb.EJB;
+
+import org.jboss.resteasy.spi.NotFoundException;
+
+import de.java.ejb.DrugService;
+import de.java.ejb.stats.AverageInventoryStatisticsService;
+
+public class DrugStatsResourceImpl implements DrugStatsResource {
+
+  @EJB
+  private AverageInventoryStatisticsService service;
+
+  @EJB
+  private DrugService drugService;
+
+  @Override
+  public Collection<DrugStatistic> getAllStatistics() {
+    Calendar cal = Calendar.getInstance();
+    Date toDate = cal.getTime();
+    cal.add(Calendar.DAY_OF_YEAR, -30);
+    Date fromDate = cal.getTime();
+    return service.getAllStats(fromDate, toDate);
+  }
+
+  @Override
+  public DrugStatistic getStatistic(int pzn) {
+
+    validateDrugExists(pzn);
+
+    Calendar cal = Calendar.getInstance();
+    Date toDate = cal.getTime();
+    cal.add(Calendar.DAY_OF_YEAR, -30);
+    Date fromDate = cal.getTime();
+    return service.getStatistic(pzn, fromDate, toDate);
+  }
+
+  private void validateDrugExists(int pzn) {
+    if (drugService.getDrug(pzn) == null)
+      throw new NotFoundException(String.format("Drug with pzn %s does not exist here", pzn));
+  }
+
+}
