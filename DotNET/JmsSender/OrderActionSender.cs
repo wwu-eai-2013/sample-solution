2fdcc233f72cc90b37c8cf821b40de0bf84a2406
Jan Ernsting <jan.ernsting@gmail.com>
2013-07-09 20:38:14 +0200

    Add stub for sending to hornetQ

---
+++ DotNET/JmsSender/OrderActionSender.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apache.NMS;
+// import stomp as well (somehow NMSConnectionFactory won't work in this context)
+using Apache.NMS.Stomp;
+
+namespace JmsSender
+{
+    public static class OrderActionSender
+    {
+        // virtualbox host ip address (10.0.2.2)
+        private static string HOST = "10.0.2.2";
+
+        // when running on the same system use localhost
+        // private static string HOST = "localhost";
+
+        static void Main(string[] args)
+        {
+        }
+
+        public static void Open(int orderId)
+        {
+            string messageText = String.Format("OPEN;{0}", orderId);
+
+            SendMessage(messageText);
+        }
+
+        private static void SendMessage(string messageText)
+        {
+            Uri connectUri = new Uri(String.Format("stomp:tcp://{0}:61613", HOST));
+            IConnectionFactory factory = new ConnectionFactory(connectUri);
+
+            using (IConnection connection = factory.CreateConnection())
+            using (ISession session = connection.CreateSession())
+            using (IMessageProducer producer = session.CreateProducer(session.GetDestination("topic://OrderActions")))
+            {
+                ITextMessage message = producer.CreateTextMessage();
+                message.Properties.SetString("subsidiary", "CSharpe");
+                message.Text = messageText;
+                producer.Send(message);
+            }
+        }
+
+        public static void Add(int pzn, int quantity, int orderId)
+        {
+            Console.WriteLine("add");
+        }
+    }
+}
