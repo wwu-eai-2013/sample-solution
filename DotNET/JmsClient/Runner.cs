80c07ca06962dbc47bcfe4f6958fdf07ae8d90d0
Jan Ernsting <jan.ernsting@gmail.com>
2013-07-09 11:47:12 +0200

    Add preliminary order action listener

--- DotNET/JmsClient/Runner.cs
+++ DotNET/JmsClient/Runner.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Apache.NMS;
 
 namespace JmsClient
 {
-    public class Runner
+    class Runner
     {
+        // virtualbox host ip address (10.0.2.2)
+        private static string HOST = "10.0.2.2";
+
+        // when running on the same system use localhost
+        // private static string HOST = "localhost";
+
+        private static ISession session;
+
+        static void Main(string[] args)
+        {
+            Uri connectUri = new Uri(String.Format("stomp:tcp://{0}:61613", HOST));
+            IConnectionFactory factory = new NMSConnectionFactory(connectUri);
+
+            using (IConnection connection = factory.CreateConnection())
+            using (session = connection.CreateSession())
+            {
+                // durable consumers require a clientId
+                connection.ClientId = "CSharpe";
+
+                using (IMessageConsumer orderActionConsumer = session.CreateDurableConsumer(((ITopic)session.GetDestination("topic://OrderActions")), "CSharpeOrderActionListenener", null, false))
+                {
+                    connection.Start();
+
+                    orderActionConsumer.Listener += new MessageListener(OrderActionListener.OnOrderAction);
+                    Console.WriteLine("orderActionConsumer started, waiting for messages...");
+
+                    Console.WriteLine("(Press ENTER to stop.)");
+                    Console.ReadLine();
+                }
+            }
+        }
     }
 }
