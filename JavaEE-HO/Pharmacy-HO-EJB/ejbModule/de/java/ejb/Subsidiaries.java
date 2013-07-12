207c73e52acd51d345462720858bc6559c12ffea
Jan Ernsting <jan.ernsting@gmail.com>
2013-07-12 15:36:16 +0200

    Unify display names

--- JavaEE-HO/Pharmacy-HO-EJB/ejbModule/de/java/ejb/Subsidiaries.java
+++ JavaEE-HO/Pharmacy-HO-EJB/ejbModule/de/java/ejb/Subsidiaries.java
@@ -1,11 +1,13 @@
 package de.java.ejb;
 
+import de.java.ejb.jms.domain.Subsidiary;
+
 public class Subsidiaries {
 
-  public static final String JAVA_DISPLAY_NAME = "JaVa";
+  public static final String JAVA_DISPLAY_NAME = Subsidiary.JaVa.toString();
   public static final String JAVA_HOST = "http://localhost:8080/Pharmacy-Web/api/";
 
-  public static final String CSHARPE_DISPLAY_NAME = "C.Sharpe";
+  public static final String CSHARPE_DISPLAY_NAME = Subsidiary.CSharpe.toString();
   public static final String CSHARPE_HOST = "http://localhost:49406/";
 
 }
